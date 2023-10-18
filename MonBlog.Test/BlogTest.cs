f34e269fad0ceae710dcdc1e284c3fadf2b66a31
Jerome robichez <73989955+Jeromerobichez@users.noreply.github.com>
2023-10-18 13:21:06 +0200

    2 fichier HTML et route dynamique

--- MonBlog.Test/BlogTest.cs
+++ MonBlog.Test/BlogTest.cs
@@ -22,7 +22,38 @@ public class BlogTest : IClassFixture<WebApplicationFactory<Program>>
         var response = await client.GetAsync("/");
 
         // ALORS on obtient un Hello World dans le premier titre
+        //Assert.SelectorContains("Hello, World", "body>h1", html);
+
+        response.EnsureSuccessStatusCode();
         var content = await response.Content.ReadAsStreamAsync();
-        Assert.HtmlContainsAt("Hello, World", content, "body>h1");
+        var html = new HtmlDocument();
+        html.Load(content);
+
+        var h1 = html.DocumentNode.QuerySelector("body>h1");
+        Assert.Contains("Hello, World", h1.InnerText);
+    }
+    [Theory]
+    [InlineData("1")]
+    [InlineData("2")]
+    [InlineData("3")]
+    [InlineData("MonBlog super article")]
+    public async Task Article(string id)
+    {
+        var client = _factory.CreateClient();
+
+        // QUAND on fait GET /article/1
+        var response = await client.GetAsync($"/article/{id}");
+
+        // ALORS on obtient la page de l'article numéro 1 dont le h1 est article 1
+
+        //Assert.SelectorContains("Hello, World", "body>h1", html);
+
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStreamAsync();
+        var html = new HtmlDocument();
+        html.Load(content);
+
+        var h1 = html.DocumentNode.QuerySelector("body>h1");
+        Assert.Contains($"Article {id}", h1.InnerText);
     }
 }
--- MonBlog/Controllers/BlogController.cs
+++ MonBlog/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
 
 namespace MonBlog.Controllers
 {
@@ -10,6 +11,13 @@ namespace MonBlog.Controllers
         public IActionResult Get()
         {
             return Content("<html><body><h1>Hello, World</h1></body></html>", "text/html");
+        }
+        [HttpGet("/article/{id}")]
+        public IActionResult GetArticle(string id)
+        {
+            string filePath = $"C:\\Users\\jerome\\Source\\Repos\\MonBlog_CDAHuman_2023\\MonBlog\\articles\\{id}.html";
+            string contentType = "text/html";
+            return File(System.IO.File.ReadAllBytes(filePath), contentType);
         }
     }
 }
